2ea2c9124c4c2b42b000e757257df97bfd1b827e
vaneApaza <vanesa.apaza.ruiz@gmail.com>
2023-11-14 20:04:23 -0300

    se agrego formulario

---
+++ vet-guardias-app/src/components/FormularioVeterinaria.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+<form class="row g-3">
+  <div class="col-md-6">
+    <label for="veterinaria" class="form-label">Nombre de veterinaria</label>
+    <input type="text" class="form-control" id="veterinaria">
+  </div>
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="direccion" class="form-label">Direccion</label>
+    <input type="text" class="form-control" id="direccion">
+  </div>
+  <div class="col-12">
+    <label for="telefono" class="form-label">Telefono</label>
+    <input type="tel" class="form-control" id="telefono">
+  </div>
+  <div class="mb-3">
+  <label for="formFile" class="form-label">Constancia de veterinaria</label>
+  <input class="form-control" type="file" id="formFile">
+</div>
+
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Registrar</button>
+  </div>
+</form>
+
+</template>
