c414d81fa7be702ef90e24f0e0fb762fbc34be5c
ivanFlor3s <ifloresmanc96@gmail.com>
2023-11-17 01:00:02 -0300

    add midleware validate jwt

--- src/routes/veterinarias.router.js
+++ src/routes/veterinarias.router.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import * as veterinaria from "../controllers/veterinaria.controller.js";
-
+import {validarJwt} from "../middlewares/validate-jwt.js"
 export const veterinariasRouter = Router();
 
 veterinariasRouter.post("/", veterinaria.create )
 veterinariasRouter.get("/", veterinaria.getAll )
-veterinariasRouter.delete("/:id", veterinaria.remove )
+veterinariasRouter.delete("/:id",[validarJwt], veterinaria.remove )
 veterinariasRouter.put("/:id", veterinaria.update )
