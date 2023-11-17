f83546ffab7f9036efef7e357ded870bcccb9d26
ivanFlor3s <ifloresmanc96@gmail.com>
2023-11-17 00:21:53 -0300

    user login

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,4 +4,5 @@ import * as UserController from "../controllers/users.controller.js";
 export const router = Router();
 
 router.post('/', UserController.create)
+router.post('/login', UserController.login)
 // router.post("/", createUser )
