b1055f3fb5ec2d1010e4cf4353e8c77a3d9ff48d
ivanFlor3s <ifloresmanc96@gmail.com>
2023-11-17 21:25:33 -0300

    Set reletion review

--- src/models/relations.js
+++ src/models/relations.js
@@ -1,7 +1,12 @@
+import { Review } from "./review.js";
 import { User } from "./user.js";
 import { Veterinaria } from "./veterinaria.js";
 
 export const configRelations = () => {
     User.hasOne(Veterinaria );
     Veterinaria.belongsTo(User);
+
+    User.belongsToMany(Veterinaria, { through: Review, foreignKey: 'UserId' });
+    Veterinaria.belongsToMany(User, { through: Review, foreignKey: 'VeterinariaId' });
+
 }
